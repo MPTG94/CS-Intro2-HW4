dffd1c80bf6fdbeeb04533f0fd9900ccb5a95995
Mor Paz <1912.mor@gmail.com>
2020-06-07 22:00:08 +0300

    Finish Q2, need to update test runner

--- hw4q2.c
+++ hw4q2.c
@@ -12,7 +12,6 @@
 #define UPPERCASE_A 'A'
 #define UPPERCASE_Z 'Z'
 #define LOWERCASE_A 'a'
-#define LOWERCASE_Z 'z'
 
 /* Function Declarations */
 char encrypt(unsigned char key[KEY_SIZE], char ch);
@@ -38,7 +37,6 @@ void print_words(char *words[], int num_words) {
 
 
 int main() {
-    //setbuf(stdout, 0);
     unsigned char key[KEY_SIZE] = {
 
             '>', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '~', 'S', 'D',
@@ -69,73 +67,37 @@ char encrypt(unsigned char key[KEY_SIZE], char ch) {
     if (ch >= UPPERCASE_A && ch <= UPPERCASE_Z) {
         return key[ch - UPPERCASE_A];
     } else {
-        return key[ch - LOWERCASE_A - (LOWERCASE_A - UPPERCASE_Z) + 1];
+        return key[ch - LOWERCASE_A + 26];
     }
 }
 
 int read_words(char *words[], int size, int max_str_len) {
     int count = 0;
-    int currNum = 0;
+    // Allocating temporary string to store all input strings
     char *temp = malloc(sizeof(char) * max_str_len);
     *temp = 0;
     if (!temp) {
         return -1;
     }
-    char ch;
-    while (scanf("%c", &ch) && count < size) {
-        if (ch == ' ' || ch == '\n') {
-            // This is the end of a word or the end of input
-            int numLetters = get_string_length(temp);
-            char *nWord = malloc(sizeof(char) * (numLetters + 1));
-            if (!nWord) {
-                free(temp);
-                for (int i = 0; i < count; ++i) {
-                    free(words[i]);
-                }
-                return -1;
+    while (scanf("%s", temp) != EOF && count < size) {
+        int currLength = get_string_length(temp);
+        char *newWord = malloc(sizeof(char) * (currLength + 1));
+        if (!newWord) {
+            free(temp);
+            for (int i = 0; i < count; ++i) {
+                free(words[i]);
             }
-            for (int i = 0; i < numLetters; ++i) {
-                nWord[i] = temp[i];
-            }
-            // adding terminating zero
-            nWord[numLetters] = 0;
-            words[count] = nWord;
-            count++;
-            currNum = 0;
-            if (ch == '\n') {
-                // We are done taking words as input
-                break;
-            }
-        } else {
-            // This is not the end of a word, and also not the end of input,
-            // adding new char to the current word
-            temp[currNum] = ch;
-            currNum++;
+            return -1;
         }
+        for (int i = 0; i < currLength; ++i) {
+            newWord[i] = temp[i];
+        }
+        // Adding terminating zero
+        newWord[currLength] = 0;
+        words[count] = newWord;
+        count++;
     }
-//    while (scanf("%s", temp) && count < size) {
-//        printf("%s",temp);
-//        if (temp[0] == '\n') {
-//            printf("empty");
-//            count--;
-//            break;
-//        }
-//        int numLetters = get_string_length(temp);
-//        words[count] = malloc(sizeof(char) * (numLetters + 1));
-//        if (!words[count]) {
-//            free(temp);
-//            for (int i = 0; i < count; ++i) {
-//                free(words[i]);
-//            }
-//            return -1;
-//        }
-//        for (int i = 0; i < numLetters; ++i) {
-//            words[count][i] = temp[i];
-//        }
-//        // adding terminating zero
-//        words[count][numLetters] = 0;
-//        count++;
-//    }
+    // Freeing temporary string
     free(temp);
     return count;
 }
